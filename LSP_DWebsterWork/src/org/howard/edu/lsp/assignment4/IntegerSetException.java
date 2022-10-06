424cf648ceaa4b8d09b1dd072e88b6b36a79b25b
dgswe <dgswe@10.188.156.134>
2022-10-06 06:06:17 -0400

    Submission for Assignment 4

---
+++ LSP_DWebsterWork/src/org/howard/edu/lsp/assignment4/IntegerSetException.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment4;
+
+/**
+ * Daniel Webster
+ * @author 29Daniel
+ *
+ */
+/**
+ * Exception to be thrown when an IntegerSet method is unable
+ * to be run
+ */
+public class IntegerSetException extends Exception {
+	/**
+	 * Default Constructor
+	 */
+    public IntegerSetException() {};
+}
